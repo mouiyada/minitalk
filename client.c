bd0f1164ff43e7ed09bcbefc00b71a9570d254f3
mouiyada <osaraman@hotmail.com>
2025-09-10 17:56:09 +0900

    Add files via upload

--- client.c
+++ client.c
@@ -6,14 +6,13 @@
 /*   By: kyamada <kyamada@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 16:19:34 by kyamada           #+#    #+#             */
-/*   Updated: 2025/09/10 16:19:55 by kyamada          ###   ########.fr       */
+/*   Updated: 2025/09/10 17:44:03 by kyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <signal.h>
-#include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 
 void	send_signal(int pid, unsigned char character)
 {
@@ -24,9 +23,21 @@ void	send_signal(int pid, unsigned char character)
 	{
 		i--;
 		if (((character >> i) & 1) == 0)
-			kill(pid, SIGUSR2);
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				ft_printf("Failed to send signal to PID %d\n", pid);
+				exit(1);
+			}
+		}
 		else
-			kill(pid, SIGUSR1);
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				ft_printf("Failed to send signal to PID %d\n", pid);
+				exit(1);
+			}
+		}
 		usleep(200);
 	}
 }
@@ -43,6 +54,11 @@ int	main(int argc, char *argv[])
 		exit(0);
 	}
 	server_pid = ft_atoi(argv[1]);
+	if (server_pid <= 0)
+	{
+		ft_printf("Invalid PID: %s\n", argv[1]);
+		exit(0);
+	}
 	message = argv[2];
 	i = 0;
 	while (message[i])
--- server.c
+++ server.c
@@ -6,17 +6,13 @@
 /*   By: kyamada <kyamada@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 16:20:07 by kyamada           #+#    #+#             */
-/*   Updated: 2025/09/10 16:20:10 by kyamada          ###   ########.fr       */
+/*   Updated: 2025/09/10 17:53:48 by kyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-
-#define END_TRANSMISSION '\0'
 
 void	handle_signal(int signal)
 {
@@ -29,7 +25,7 @@ void	handle_signal(int signal)
 	bit_index++;
 	if (bit_index == 8)
 	{
-		if (current_char == END_TRANSMISSION)
+		if (current_char == '\0')
 			write(1, "\n", 1);
 		else
 			write(1, &current_char, 1);
@@ -41,8 +37,16 @@ void	handle_signal(int signal)
 int	main(void)
 {
 	ft_printf("%d\n", getpid());
-	signal(SIGUSR1, handle_signal);
-	signal(SIGUSR2, handle_signal);
+	if (signal(SIGUSR1, handle_signal) == SIG_ERR)
+	{
+		ft_printf("Error: signal() failed\n");
+		return (1);
+	}
+	if (signal(SIGUSR2, handle_signal) == SIG_ERR)
+	{
+		ft_printf("Error: signal() failed\n");
+		return (1);
+	}
 	while (1)
 		pause();
 	return (0);
